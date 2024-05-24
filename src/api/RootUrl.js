cd79eb6f89ca1a0c4fe0d0ed2f349a63ccbee163
hyeji111544 <hyeji111544@gmail.com>
2024-05-24 13:00:40 +0900

    태스트

--- src/api/RootUrl.js
+++ src/api/RootUrl.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const RootUrl = async () => {
-    const URL = 'https://zeropie.netlify.app/onepie';
+    const URL = 'https://15.165.24.202/onepie';
 
     return RootUrl;
 };
