94bb38d5ead602056d998cf682df74dafe2045a6
NAIMJAE <skdlawo5985@gamil.com>
2024-06-03 22:47:54 +0900

    기본설정 페이지 디자인

---
+++ src/components/admin/DptConfigComponent.js
@@ -0,0 +1,54 @@
+import { faUsersViewfinder } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React, { useState } from 'react'
+import DptIconOptionComponent from './DptIconOptionComponent'
+import CreateDtpModal from '../modal/CreateDtpModal'
+
+const DptConfigComponent = ({dptValue, handleChangeDpt}) => {
+
+    /** 부서 생서 모달 */
+    const [createDpt, setCreateDpt] = useState(false);
+
+    const handelOpenModal = () => {
+        setCreateDpt(true);
+    }
+
+    const handelColseModal = () => {
+        setCreateDpt(false);
+    }
+
+
+
+    
+
+  return (
+    <div className='contentBox boxStyle4'>
+        <div className="contentTitle font30 alignL">부서 설정</div>
+        <div className="contentInfo alignL">회사 내 부서 이름, 종류 변경 가능합니다.</div>
+
+        <div className='scrollBox'>
+            <div className='scrollRow configRow'>
+                <div>1</div>
+                <div>
+                    설정된 아이콘
+                </div>
+                <div>
+                    <input type="text" value={dptValue} onChange={handleChangeDpt}/>
+                </div>
+                <div className='configBtn'>
+                    <button>수정</button>
+                    <button>삭제</button>
+                </div>
+            </div>
+        </div>
+
+        <div className='configBtn'>
+            <button onClick={handelOpenModal}>생성</button>
+        </div>
+
+        {createDpt && <CreateDtpModal handelColseModal={handelColseModal}/>}
+    </div>  
+  )
+}
+
+export default DptConfigComponent
