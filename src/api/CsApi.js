770937a48693bd7a2bd15c48343ea4d519bca7cc
oharam <oharam0413@gmail.com>
2024-06-04 17:27:37 +0900

    csView.done

--- src/api/CsApi.js
+++ src/api/CsApi.js
@@ -30,4 +30,41 @@ export const getCsView = async (data) => {
 };
 
 
+//댓글달기
+export const postCsComment = async (data) => {
+    console.log('글 목록', data);
+    const response = await axios.post(`${rootURL}/answer`,data);
+
+    return response.data;
+};
+
+
+//댓글불러오기
+export const getcsComment = async (data) => {
+    console.log('글 목록', data);
+    const response = await axios.get(`${rootURL}/answerList?csNo=${data}`);
+
+    return response.data;
+};
+
+
+
+//댓글삭제
+export const getCommentDelete = async (data) => {
+    console.log('글 목록', data);
+    const response = await axios.get(`${rootURL}/answerDelete?csComNo=${data}`);
+
+    return response.data;
+};
+
+
+//게시글 삭제
+export const getDeleteCsView = async (data) => {
+    console.log('글 목록', data);
+
+    const response = await axios.get(`${rootURL}/delete?csNo=${data}`);
+
+    return response.data;
+};
+
 
--- src/components/common/comment/CommentListComponent.js
+++ src/components/common/comment/CommentListComponent.js
@@ -1,23 +1,50 @@
 import React from 'react'
+import { getCommentDelete } from '../../../api/CsApi';
+import { RootUrl } from '../../../api/RootUrl';
+import Moment from 'moment';
+import "moment/locale/ko";
 
-const CommentListComponent = ({ comment }) => {
+const CommentListComponent = ({ comment,name,csNo }) => {
+
+    const deleteHandler = async (e)=>{
+
+        e.preventDefault();
+
+        const confirmed = window.confirm('정말 삭제하시겠습니까?');
+
+        if(confirmed){
+            const no = e.target.dataset.id;
+
+            console.log("누른 버튼의 댓글 id : ",no);
+
+            await getCommentDelete(no);
+
+            const url = "/csView?csNo=" + csNo;
+            window.location.href=url;   
+        } 
+    }
+
+
   return (
     <>
         {comment ? comment.map((each, index) => (
             <div className='commentRow commentColumn'>
                 <div>
-                    <img src="../images/iconSample3.png" alt="" />
-                    <div className='commentContent'>
+                <div key={index}>
+                    <img src={`${RootUrl()}/images/${each.stfImg}`} alt='image from spring'/>    
+                </div>       
+                       <div className='commentContent'>
                         <div className='commentTitle'>
-                            <p>홍길동</p>
-                            <p>{each.commentRdate}</p>
+                            <p>{each.stfName}</p>
+                            <p>{/* 날짜 포맷(import 수동) / npm install moment --save */}
+                    {Moment(each.csComRdate).format('YYYY-MM-DD HH:MM:DD')}</p>
                         </div>
-                        <textarea name="" id="" value={each.commentCnt}></textarea>
+                        <textarea readOnly name="" id="" value={each.csComContent}></textarea>
                     </div>
                 </div>
 
                 <div style={{alignSelf:"self-end"}}>
-                    <button>삭제</button>
+                    <button data-id={each.csComNo} onClick={deleteHandler}>삭제</button>
                 </div>
             </div>
         )) : (
