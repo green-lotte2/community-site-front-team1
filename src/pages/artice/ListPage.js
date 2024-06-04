6dec657bb67af4ba46c4e2ea65207647483d95f2
123rhkddms2 <123rhkddms2@gmail.com>
2024-06-04 17:45:01 +0900

    list페이지 card에 건 이벤트 삭제

--- src/pages/artice/ListPage.js
+++ src/pages/artice/ListPage.js
@@ -100,10 +100,6 @@ const ListPage = () => {
         }
     };
 
-    const handleCard = () => {
-        navigate(`/card?articleCateNo=${articleCateNo}&pg=1`);
-    };
-
     return (
         <MainLayout>
             <div className="contentBox boxStyle7">
@@ -114,7 +110,7 @@ const ListPage = () => {
                 <div className="contentColumn">
                     <div className="listType">
                         <span className="listOn">리스트</span>
-                        <span onClick={handleCard}>카드형</span>
+                        <span>카드형</span>
                     </div>
                 </div>
 
