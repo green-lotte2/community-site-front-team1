6364864293355bacfdc64bdaeb5486e0eece23ed
123rhkddms2 <123rhkddms2@gmail.com>
2024-06-05 17:02:53 +0900

    article 이미지,파일첨부 부분

--- src/components/article/SearchComponent.js
+++ src/components/article/SearchComponent.js
@@ -1,36 +1,66 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const SearchComponent = ({ onSearch }) => {
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [sort, setSort] = useState('default');
-  const [type, setType] = useState('title');
-  const [keyword, setKeyword] = useState('');
+  const [isCheckedDefault, setIsCheckedDefault] = useState(false);
+  const [isCheckedLatest, setIsCheckedLatest] = useState(false);
+  const [isCheckedHit, setIsCheckedHit] = useState(false);
+
+  const [searchParams, setSearchParams] = useState({
+    startDate: '',
+    endDate: '',
+    sort: 'default',
+    type: 'title',
+    keyword: ''
+  });
 
   const handleSearch = () => {
-    const isValidDate = (dateString) => {
-      const regex = /^\d{4}-\d{2}-\d{2}$/;
-      return dateString.match(regex) !== null;
-    };
+    onSearch(searchParams);
+  };
 
-    if (startDate && !isValidDate(startDate)) {
-      console.error('Invalid start date format');
-      return;
-    }
+  useEffect(() => {
+    handleSearch();
+  }, [searchParams.sort]);
 
-    if (endDate && !isValidDate(endDate)) {
-      console.error('Invalid end date format');
-      return;
-    }
+  const handleChangeDefault = () => {
+    setSearchParams((prevParams) => ({
+      ...prevParams,
+      sort: 'default'
+    }));
 
-    const searchParams = {
-      startDate,
-      endDate,
-      sort,
-      type,
-      keyword,
-    };
-    onSearch(searchParams); // 부모 컴포넌트로 검색 매개변수 전달
+    setIsCheckedDefault(true);
+    setIsCheckedLatest(false);
+    setIsCheckedHit(false);
+  };
+
+  const handleChangeLatest = () => {
+    setSearchParams((prevParams) => ({
+      ...prevParams,
+      sort: 'latest'
+    }));
+
+    setIsCheckedLatest(true);
+    setIsCheckedDefault(false);
+    setIsCheckedHit(false);
+  };
+
+  const handleChangeHit = () => {
+    setSearchParams((prevParams) => ({
+      ...prevParams,
+      sort: 'hit'
+    }));
+
+    setIsCheckedHit(true);
+    setIsCheckedDefault(false);
+    setIsCheckedLatest(false);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setSearchParams((prevParams) => ({
+      ...prevParams,
+      [name]: value
+    }));
   };
 
   return (
@@ -38,41 +68,44 @@ const SearchComponent = ({ onSearch }) => {
       <div className="searchRow">
         <input 
           type="date" 
-          value={startDate} 
-          onChange={(e) => setStartDate(e.target.value)} 
+          name="startDate" 
+          value={searchParams.startDate} 
+          onChange={handleInputChange} 
         /> ~ 
         <input 
           type="date" 
-          value={endDate} 
-          onChange={(e) => setEndDate(e.target.value)} 
+          name="endDate" 
+          value={searchParams.endDate} 
+          onChange={handleInputChange} 
         />
         <label>
           <input 
             type="checkbox" 
-            checked={sort === 'default'} 
-            onChange={() => setSort('default')} 
+            checked={isCheckedDefault} 
+            onChange={handleChangeDefault} 
           /> 기본정렬
         </label>
         <label>
           <input 
             type="checkbox" 
-            checked={sort === 'hit'} 
-            onChange={() => setSort('hit')} 
+            checked={isCheckedHit} 
+            onChange={handleChangeHit} 
           /> 조회순
         </label>
         <label>
           <input 
             type="checkbox" 
-            checked={sort === 'latest'} 
-            onChange={() => setSort('latest')} 
+            checked={isCheckedLatest} 
+            onChange={handleChangeLatest} 
           /> 최신순
         </label>
       </div>
 
       <div className="searchRow">
         <select 
-          value={type} 
-          onChange={(e) => setType(e.target.value)}
+          name="type" 
+          value={searchParams.type} 
+          onChange={handleInputChange}
         >
           <option value="title">제목</option>
           <option value="content">내용</option>
@@ -81,8 +114,9 @@ const SearchComponent = ({ onSearch }) => {
         </select>
         <input 
           type="text" 
-          value={keyword} 
-          onChange={(e) => setKeyword(e.target.value)} 
+          name="keyword" 
+          value={searchParams.keyword} 
+          onChange={handleInputChange} 
         />
         <input 
           type="submit" 
