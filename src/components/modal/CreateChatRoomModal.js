cc880c73c0696e8bd279260e5e184911a034d465
oharam0413 <oharam0413@gmail.com>
2024-06-09 23:58:21 +0900

    Chatting.roomCreate.done

--- src/components/modal/CreateChatRoomModal.js
+++ src/components/modal/CreateChatRoomModal.js
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { postCreateRoom } from '../../api/ChatApi'
+import { useNavigate } from 'react-router-dom';
 
-const CreateChatRoomModal = ({handelColseModal}) => {
+const CreateChatRoomModal = ({handelColseModal, handleCreateRoom}) => {
+
+    const [titleInfo,setTitleInfo] = useState({
+        name:'',
+        geust:''
+
+    });
+
+    const inputTitle = (e)=>{
+        setTitleInfo({ ...titleInfo, [e.target.name]: e.target.value });
+
+    }
+    console.log("입력한 방 이름 : ",titleInfo.name);
+
+
+    const submitHandler = async ()=>{
+
+        try {
+            await handleCreateRoom(titleInfo.name);
+          
+        } catch (error) {
+            console.error('Error creating chat room:', error);
+        }
+    }
+
   return (
     <div className="modlaBack modalClose">
         <div className="modalBox">
@@ -15,7 +41,7 @@ const CreateChatRoomModal = ({handelColseModal}) => {
                 <div className="modalRow">
                     <div className="maR30">이름</div>
                     <div>
-                        <input type="text" />
+                        <input type="text" name="name" value ={titleInfo.name} onChange={inputTitle}/>
                     </div>
                 </div>
             </div>
@@ -44,7 +70,7 @@ const CreateChatRoomModal = ({handelColseModal}) => {
 
             <div className="modalRow">
                 <button className="modalClose" onClick={handelColseModal}>취소</button>
-                <input type="submit" value="생성"/>
+                <input type="submit" value="생성" onClick={submitHandler}/>
             </div>
         </div>
     </div>
--- src/components/private/chat/ChatListComponent.js
+++ src/components/private/chat/ChatListComponent.js
@@ -1,12 +1,14 @@
 import { faMessage, faSquarePlus } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CreateChatRoomModal from '../../modal/CreateChatRoomModal'
+import { getRoomList,postCreateRoom } from '../../../api/ChatApi'
 
 const ChatListComponent = () => {
 
     /** 채팅방 생성 모달 관리 */
     const [openCreateChatRoom, setOpenCreateChatRoom] = useState(false);
+    const [roomList, setRoomList] = useState([]);
 
     const handelOpenModal = () => {
         setOpenCreateChatRoom(true);
@@ -15,6 +17,34 @@ const ChatListComponent = () => {
     const handelColseModal = () => {
         setOpenCreateChatRoom(false);
     }
+
+   
+    const fetchRoomList = async () => {
+            try {
+                const response = await getRoomList();
+                setRoomList(response); 
+                console.log(response);
+            } catch (error) {
+                console.error('Error fetching room list:', error);
+            }
+    };
+    useEffect(() => {
+
+        fetchRoomList();
+
+    }, []);
+
+    const createChatRoom = async (name) => {
+        try {
+            await postCreateRoom(name);
+            // 생성 후 방 목록 갱신
+            fetchRoomList();
+            // 모달 닫기
+            handelColseModal();
+        } catch (error) {
+            console.error('Error creating chat room:', error);
+        }
+    };
 
   return (
     <div className="contentBox boxStyle9">
@@ -33,31 +63,19 @@ const ChatListComponent = () => {
             </div>
         </div>
 
-        <div className='chatRoomList'>
+{/*여기에 디비에 저장해둔 방 리스트들 출력 */}
+
+        {roomList.map(room => (
+                <div key={room.roomId} className='chatRoomList'>
             <FontAwesomeIcon icon={faMessage} style={{color: "#13a8ae",}} />
             <div>
-                <p>회의방</p>
+                <p>{room.name}</p>
             </div>
             <span className='ballCount'>1</span>
         </div>
+        ))}
             
-        <div className='chatRoomList'>
-            <FontAwesomeIcon icon={faMessage} style={{color: "#13a8ae",}} />
-            <div>
-                <p>회의방</p>
-            </div>
-            <span className='ballCount'>1</span>
-        </div>
-
-        <div className='chatRoomList'>
-            <FontAwesomeIcon icon={faMessage} style={{color: "#13a8ae",}} />
-            <div>
-                <p>회의방</p>
-            </div>
-            <span className='ballCount'>1</span>
-        </div>
-
-        {openCreateChatRoom && <CreateChatRoomModal handelColseModal={handelColseModal}/>}
+        {openCreateChatRoom && <CreateChatRoomModal handleCreateRoom={createChatRoom} handelColseModal={handelColseModal}/>}
 
     </div>
   )
