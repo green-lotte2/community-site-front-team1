82f02a8593932c1a5e1f7f48591106a6b497c84e
NAIMJAE <153374452+NAIMJAE@users.noreply.github.com>
2024-06-12 17:26:26 +0900

    페이지 소켓 연결까지 완료

--- src/components/private/doc/DocEditor.js
+++ src/components/private/doc/DocEditor.js
@@ -1,80 +1,117 @@
-import React, { useRef } from "react";
-import { createReactEditorJS } from "react-editor-js";
-import { EDITOR_JS_TOOLS } from "./DocEditorTool";
-import { saveDoc } from "../../../api/DocApi";
+import { BlockNoteView } from "@blocknote/mantine";
+import { useCreateBlockNote } from "@blocknote/react";
+import { useEffect, useRef, useState } from "react";
+import { WebrtcProvider } from "y-webrtc";
+import * as Y from "yjs";
+import "@blocknote/core/fonts/inter.css";
+import "@blocknote/mantine/style.css";
 
 const DocEditor = () => {
 
-    const test = {
-        blocks: [
-          {
-            id: "121ff12",
-            type: "header",
-            data: {
-              text: "안녕하세요",
-              level: "2"
+    const doc = new Y.Doc();
+    const provider = useRef(null);
+    const [ex, setEx] = useState([
+        {
+          type: "paragraph",
+          content: "Welcome to this demo!",
+        },
+      ],)
+
+    /** 소켓 연결 useEffect */
+    useEffect(() => {
+        // 컴포넌트가 마운트될 때 WebrtcProvider를 생성합니다.
+        provider.current = new WebrtcProvider('test111111', doc, { signaling: ['ws://127.0.0.1:8080/onepie/testaa'] });
+        // 컴포넌트가 언마운트될 때 provider를 정리합니다.
+        return () => {
+            provider.current.destroy();
+        };
+    }, []);
+  
+    /** 에디터 기본 설정 (collaboration) */
+    const editor = useCreateBlockNote({
+        defaultStyles: true,
+        uploadFile: (file) => Promise.resolve(''),
+        collaboration: {
+            provider: provider.current,
+            fragment: doc.getXmlFragment("document-store"),
+            user: {
+                name: "My Username",
+                color: "#ff0000",
             },
-          },
-          {
-            id: "122ff12",
-            type: "code",
-            data: {
-              code: "안녕하세요",
-            },
-          },
-        ],
-        time: 1635603431943,
-        version: "2.22.2",
-      };
+        },
+    });
 
-    const ReactEditorJS = createReactEditorJS({
-        holder: "editor-container",
-      });
+/** 저장 테스트 */
+    async function saveToStorage(jsonBlocks) {
+        // Save contents to local storage. You might want to debounce this or replace
+        // with a call to your API / database.
+        localStorage.setItem("editorContent", JSON.stringify(jsonBlocks));
+    }
 
-      const editorCore = useRef(null);
-
-      const handleInitialize = React.useCallback((instance) => {
-        editorCore.current = instance;
-      }, []);
-
-      const check = async () => {
-        try {
-            const savedData = await editorCore.current.save();
-            console.log('data:', savedData);
-          } catch (error) {
-            console.error('failed:', error);
-          }
+    async function loadFromStorage() {
+        // Gets the previously stored editor contents.
+        const storageString = localStorage.getItem("editorContent");
+        return storageString
+          ? JSON.parse(storageString)
+          : undefined;
       }
 
-    /** 내용 저장 핸들러 */
-    const handleSave = async () => {
-        try {
-            const docData = await editorCore.current.save();
-            console.log('data:', docData);
+      /** 페이지 로드될 때 스토리지에서 꺼내와서 editor 업데이트 */
+      useEffect(() => {
+          loadFromStorage().then((data) => {
+            console.log(data)
+            try {
+                for(let i=0 ; i < 1; i++){
+                    console.log(data[i])
+                    console.log(data[i].id)
+                    editor.insertBlocks(data, data[i].id, "after");
+                    console.log("성공")
+                }
+            } catch (error) {
+              console.error('Error inserting inline content:', error);
+            }
+          });
+      }, []);
 
-            const formData = new FormData();
-            formData.append(docData);
+      
+/** 여기까지 */
 
-            const response = await saveDoc(formData);
-            console.log(response);
 
-        } catch (error) {
-            console.error('failed:', error);
+    /** 데이터 확인용 핸들러 2개 */
+    const [blocks, setBlocks] = useState([]);
+
+    const editorSelectHandler = () => {
+        const selection = editor.getSelection();
+        if (selection !== undefined) {
+            setBlocks(selection.blocks);
+        } else {
+            setBlocks([editor.getTextCursorPosition().block]);
         }
     }
-      
+    const check = () => {
+        console.log(editor.document)
+        console.log(doc)
+        console.log(editor)
+    }
+    /** 여기까지 */
 
   return (
-    <>
-    <ReactEditorJS
-        tools={EDITOR_JS_TOOLS}
-        onInitialize={handleInitialize}
-        defaultValue={test}
-    />
-    <button onClick={check}>확인</button>
-    <button onClick={handleSave}>저장</button>
-    </>
+    <div className='pageMain'>
+        <BlockNoteView
+            editor={editor}
+            onSelectionChange={editorSelectHandler}
+            onChange={() => {
+                saveToStorage(editor.document)}}
+        />
+        <div>Selection JSON:</div>
+        <div className={"item bordered"}>
+            <pre>
+                <code>{JSON.stringify(blocks, null, 2)}</code>
+            </pre>
+        </div>
+        <button onClick={check}>확인</button>
+    </div>
   )
 }
 
-export default DocEditor
+export default DocEditor;
