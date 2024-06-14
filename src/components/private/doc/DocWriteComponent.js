0d1a1b73878f857d9cc457169a325f6a7c4ea944
hyeji111544 <hyeji111544@gmail.com>
2024-06-14 12:33:22 +0900

    0614 배포

--- src/components/private/doc/DocWriteComponent.js
+++ src/components/private/doc/DocWriteComponent.js
@@ -1,29 +1,29 @@
-import { faGear, faSquarePlus } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect, useRef, useState } from 'react'
-import { BlockNoteView } from "@blocknote/mantine";
-import { useCreateBlockNote } from "@blocknote/react";
-import { WebrtcProvider } from "y-webrtc";
-import * as Y from "yjs";
-import "@blocknote/core/fonts/inter.css";
-import "@blocknote/mantine/style.css";
-import { getDocContent, saveDoc } from "../../../api/DocApi";
+import { faGear, faSquarePlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useRef, useState } from 'react';
+import { BlockNoteView } from '@blocknote/mantine';
+import { useCreateBlockNote } from '@blocknote/react';
+import { WebrtcProvider } from 'y-webrtc';
+import * as Y from 'yjs';
+import '@blocknote/core/fonts/inter.css';
+import '@blocknote/mantine/style.css';
+import { getDocContent, saveDoc } from '../../../api/DocApi';
+import { SoketUrl } from '../../../api/RootUrl';
 
-const DocWriteComponent = ({eachDocView, submitDoc}) => {
-
+const DocWriteComponent = ({ eachDocView, submitDoc }) => {
     const doc = new Y.Doc();
     const provider = useRef(null);
 
     /** 제목 관리 */
-    const [docTitle, setDocTitle] = useState("");
+    const [docTitle, setDocTitle] = useState('');
 
     /** 페이지 정보 보관하는 useRef */
     const pageRef = useRef({
-        pno: "",
-        title: "",
-        owner: "",
-        rDate: "",
-        document: "",
+        pno: '',
+        title: '',
+        owner: '',
+        rDate: '',
+        document: '',
     });
 
     /** 랜더링 시점 관리 useState */
@@ -31,9 +31,9 @@ const DocWriteComponent = ({eachDocView, submitDoc}) => {
 
     /** 소켓 연결 useEffect */
     useEffect(() => {
-        let roomId = "docId" + eachDocView;
+        let roomId = 'docId' + eachDocView;
         if (eachDocView) {
-            provider.current = new WebrtcProvider(roomId, doc, { signaling: ['ws://127.0.0.1:8080/onepie/doc'] });
+            provider.current = new WebrtcProvider(roomId, doc, { signaling: [`ws://${SoketUrl}/doc`] });
 
             return () => {
                 submitDoc(editor.document, pageRef.current);
@@ -48,10 +48,10 @@ const DocWriteComponent = ({eachDocView, submitDoc}) => {
         uploadFile: (file) => Promise.resolve(''),
         collaboration: {
             provider: provider.current,
-            fragment: doc.getXmlFragment("document-store"),
+            fragment: doc.getXmlFragment('document-store'),
             user: {
-                name: "My Username",
-                color: "#ff0000",
+                name: 'My Username',
+                color: '#ff0000',
             },
         },
     });
@@ -64,16 +64,16 @@ const DocWriteComponent = ({eachDocView, submitDoc}) => {
                 pageRef.current = response;
                 setDocTitle(response.title);
                 setLoading(true);
-                console.log(editor.document.length)
+                console.log(editor.document.length);
                 if (editor.document.length === 1) {
-                    console.log(response.document)
+                    console.log(response.document);
                     const docView = JSON.parse(response.document);
-                    editor.replaceBlocks(editor.document, docView); 
+                    editor.replaceBlocks(editor.document, docView);
                 }
             } catch (error) {
                 console.log(error);
             }
-        }
+        };
         selectDoc();
     }, []);
 
@@ -81,35 +81,28 @@ const DocWriteComponent = ({eachDocView, submitDoc}) => {
         setDocTitle(e.target.value);
         pageRef.current.title = e.target.value;
     };
-/** 여기까지 */
+    /** 여기까지 */
 
+    return (
+        <>
+            <div className="chatInfo" style={{ justifyContent: 'space-between', padding: '20px 0' }}>
+                <div>{loading && <input type="text" value={docTitle} onChange={handleChange} />}</div>
+                <label htmlFor="" style={{ display: 'flex' }}>
+                    <span>
+                        <FontAwesomeIcon icon={faSquarePlus} /> &nbsp;멤버 추가
+                    </span>
+                    <span>
+                        <FontAwesomeIcon icon={faGear} /> &nbsp;설정
+                    </span>
+                </label>
+            </div>
 
-  return (
-    <>
-    <div className="chatInfo" style={{justifyContent:"space-between", padding:"20px 0"}}>
-        <div>
-            {loading &&  
-                <input type="text" value={docTitle} onChange={handleChange}/>
-            }
-        </div>
-        <label htmlFor="" style={{display:"flex"}}>
-            <span>
-                <FontAwesomeIcon icon={faSquarePlus} /> &nbsp;멤버 추가
-            </span>
-            <span>
-                <FontAwesomeIcon icon={faGear} /> &nbsp;설정
-            </span>
-        </label>
-    </div>
+            <div className="docEditor">
+                {/** 여기 */}
+                <div className="pageMain">{pageRef && <BlockNoteView editor={editor} />}</div>
+            </div>
+        </>
+    );
+};
 
-    <div className='docEditor'>
-        {/** 여기 */}
-        <div className='pageMain'>
-            {pageRef && <BlockNoteView editor={editor}/>}
-        </div>
-    </div>
-    </>
-  )
-}
-
-export default DocWriteComponent
+export default DocWriteComponent;
